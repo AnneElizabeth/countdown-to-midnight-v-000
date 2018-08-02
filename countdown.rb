a30001d88de858839f73009b8bcf1f173e63ed2d
Anne Edmondson <edmondson.anne@gmail.com>
2018-08-02 04:43:55 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,17 @@
 def countdown(number)
   while number > 0 do
-    puts "#{number) SECONDS\(S\)!"
+    puts "#{number} SECONDS\(S\)!"
     number -= 1
   end
-  return "HAPPY NEW YEAR!"
+  puts "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+  while number > 0 do
+    puts "#{number} SECONDS\(S\)!"
+    number -= 1
+  end
+  puts "HAPPY NEW YEAR!"
 end
 
 
